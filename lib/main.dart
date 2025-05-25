23edaacbdb2cf4fc183ac0502a992b536c4343e3
Thomas Gomez <tga481@gmail.com>
2025-05-25 13:11:22 -0500

    feat(main): integración de MultiProvider para gestión de estado

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'config/injection_container.dart' as di;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'features/auth/presentation/bloc/auth_bloc.dart';
 import 'features/auth/presentation/bloc/auth_event.dart';
+import 'package:app_notes/features/notes/presentation/bloc/barrel_notes_bloc.dart';
 
 import 'package:app_notes/config/app_theme.dart';
 import 'config/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_ES');
   await di.init();
 
   runApp(const MyApp());
@@ -21,8 +24,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => di.sl<AuthBloc>()..add(CheckSessionRequested()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => di.sl<AuthBloc>()..add(CheckSessionRequested()),
+        ),
+        BlocProvider<NotesBloc>(create: (_) => di.sl<NotesBloc>()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Notas App',
